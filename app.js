3926a6a9c44491ff1ddc3a8a74f0f6e1c8ee63ad
Chicloon <Chicloon@gmail.com>
2016-08-09 22:43:48 +0900

    settingUp for angular2

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var appRoutes = require('./routes/app');
 
 var app = express();
+mongoose.connect('localhost:27017/node-angular');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,6 +29,7 @@ app.use(function(req, res, next) {
     next();
 });
 
+console.log('Server started');
 app.use('/', appRoutes);
 
 // catch 404 and forward to error handler
--- routes/app.js
+++ routes/app.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function(req, res, next) {
     res.render('index');
 });
-
-// router.post('/', function(req, res, next) {
-//     res.redirect ('/');
-// });
 
 module.exports = router;
